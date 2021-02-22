4c6e57e7ba21705d0d959ce55858d4d99677aaa4
Kate <katushkashmat@gmail.com>
2021-02-22 15:04:51 +0200

    22.02.2021

--- src/main/java/org/obrii/mit/dp2021/shmat/ShmatKateryna2021/Product.java
+++ src/main/java/org/obrii/mit/dp2021/shmat/ShmatKateryna2021/Product.java
@@ -1,21 +1,25 @@
 package org.obrii.mit.dp2021.shmat.ShmatKateryna2021;
 
 public class Product implements ProductInterface{
-    public String type = null;
+    public String name = null;
+    public String color = null;
+    public String delivery = null;
 
-    public Product(String type){
-        this.type = type;
+    public Product(String name, String color, String delivery){
+        this.name = name;
+        this.color = color;
+        this.delivery = delivery;
     }
 
     public String getName(){
-        return this.type;
+        return this.name;
     }
     
     public String getColor(){
-        return this.type;
+        return this.color;
     }
     
     public String getDelivery(){
-        return this.type;
+        return this.delivery;
     }
 }
